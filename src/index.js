fd922956b908a6bc832a0ab4377e554e075ab5a6
reZach <reZach@users.noreply.github.com>
2020-01-14 23:19:55 -0600

    setting up some build tools when we get the plugin fully working

--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
-/**
- * Fast UUID generator, RFC4122 version 4 compliant.
- * @author Jeff Ward (jcward.com).
- * @license MIT license
- * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
- **/
-var UUID = (function () {
-    var self = {};
-    var lut = [];
-    for (var i = 0; i < 256; i++) {
-        lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
-    }
-    self.generate = function () {
-        var d0 = Math.random() * 0xffffffff | 0;
-        var d1 = Math.random() * 0xffffffff | 0;
-        var d2 = Math.random() * 0xffffffff | 0;
-        var d3 = Math.random() * 0xffffffff | 0;
-        return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' +
-            lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' +
-            lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
-            lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
-    }
-    return self;
-})();
+import { UUID, mergeNested } from "./utils";
 
-
+// CONFIGS
 const defaultOptions = {
     loadPath: "/locales/{{lng}}/{{ns}}.json", // Where the translation files get loaded from
     addPath: "/locales/{{lng}}/{{ns}}.missing.json", // Where the missing translation files get generated
-    delay: 300
+    delay: 300 // Delay before translations are written to file
 };
 // Electron-specific; must match mainIpc
 const readChannel = "ReadFile";
