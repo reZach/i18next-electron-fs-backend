13acd498e5045d2abd76cfdcd738108829edcf92
danikaze <danikaze@gmail.com>
2024-08-22 00:53:36 +0900

    Improve types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,29 @@
-import { BackendModule } from "i18next";
+import { BackendModule, TFunction } from "i18next";
+import {
+    readFileRequest,
+    writeFileRequest,
+    readFileResponse,
+    writeFileResponse,
+} from './src/index.js'
+
+export type ReadFileRequest = typeof readFileRequest;
+export type WriteFileRequest = typeof writeFileRequest;
+export type ReadFileResponse = typeof readFileResponse;
+export type WriteFileResponse = typeof writeFileResponse;
+
+export interface PreloadBindings {
+    send: (channel: ReadFileRequest | WriteFileRequest, data?: any) => void;
+    onReceive: (channel: ReadFileResponse | WriteFileResponse, func: (data?: any) => void) => void;
+    onLanguageChange: (cb: (args: { lng: string }, t: TFunction) => void) => void;
+    clientOptions: {
+        environment: string | undefined;
+        platform: string;
+        resourcesPath: string;
+    }
+}
 
 export function mainBindings(ipcMain: Electron.IpcMain, browserWindow: Electron.BrowserWindow, fs: any): any;
 export function clearMainBindings(ipcMain: Electron.IpcMain): any;
-export function preloadBindings(ipcRenderer: Electron.IpcRenderer, process: NodeJS.Process): any;
+export function preloadBindings(ipcRenderer: Electron.IpcRenderer, process: NodeJS.Process): PreloadBindings;
 
 export default {} as BackendModule;
